eb6463c5179dc1cdf31dfc33b99861de65f6ffaa
linzhun <linzhun7@gmail.com>
2018-09-24 17:29:56 +0800

    添加jobs app

---
+++ jobs/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Job, JobProposal
+
+
+class JobModelAdmin(admin.ModelAdmin):
+
+    class Meta:
+        model = Job
+
+class ProposalModelAdmin(admin.ModelAdmin):
+
+    class Meta:
+        model = JobProposal
+
+
+admin.site.register(Job, JobModelAdmin)
+admin.site.register(JobProposal, ProposalModelAdmin)
+
+
