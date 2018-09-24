4c23e2da698670292bf3a27e2a97bbd630bc213d
linzhun <linzhun7@gmail.com>
2018-09-24 09:07:13 +0800

    添加URLs

--- allwork/settings.py
+++ allwork/settings.py
@@ -133,3 +133,9 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# django.contrib.auth.urls
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
