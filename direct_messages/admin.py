db6240157e69561fc6c87000c5246f3c83b510c1
linzhun <linzhun7@gmail.com>
2018-09-26 08:53:59 +0800

    在admin.py中注册 Message model

--- direct_messages/admin.py
+++ direct_messages/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Message
+
+
+class MessageAdmin(admin.ModelAdmin):
+    model = Message
+    list_display = ('id', 'sender', 'content', )
+
+
+admin.site.register(Message, MessageAdmin)
