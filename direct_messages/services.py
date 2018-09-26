1ec6caf9ed68ad18087b777617fe3e8bc3172e3e
linzhun <linzhun7@gmail.com>
2018-09-27 05:14:15 +0800

    添加services.py，定义一系列对message和conversation的操作

--- direct_messages/services.py
+++ direct_messages/services.py
@@ -3,6 +3,75 @@ from django.utils import timezone
 from django.db.models import Q
 
 from .models import Message, ChatRoom
-from .signals import message_read, message_sent
+from .signals import message_read, message_sent  # 导入Signal
 
 class MessagingService(object):
+    def send_message(self, sender, recipient, message):
+        if sender == recipient:
+            raise ValidationError("You can't send message to yourself.")
+
+        # 创建message对象
+        message = Message(sender=sender, recipient=recipient, content=str(message))
+        message.save()
+        # 通过message_sent signal 发送message
+        message_sent.send(sender=message, from_user=message.sender, to=message.recipient)
+
+        return message, 200  # 最后返回message 对象和 200 http状态码
+
+    def get_unread_messages(self, user):
+        return Message.objects.all().filter(recipient=user, read_at=None)  # 通过.filter操作数据库得到所有未读消息的集合
+
+    def read_message_formatted(self, message_id):
+        try:
+            message = Message.objects.get(id=message_id)
+            self.mark_as_read(message)
+            return message.sender.username + ": " + message.content # 以固定格式返回message
+        except Message.DoesNotExist:
+            return ""
+
+    def read_message(self, message_id):
+        # 读消息，并返回消息的文本
+        try:
+            message = Message.objects.get(id=message_id)
+            self.mark_as_read(message) # 读取消息的helper method，另外定义
+            return message.content
+        except Message.DoesNotExist:
+            return ""
+
+    def mark_as_read(self, message):
+        # 设定message读取时间，并发送已读信号，然后将修改后的message存储到数据库
+        if message.read_at is None:
+            message.read_at = timezone.now()
+            message_read.send(sender=message, from_user=message.sender, to=message.recipient)
+            message.save()
+
+    def get_conversations(self, user):
+        # 获取当前用户的对话列表
+
+        chatrooms = ChatRoom.objects.filter((Q(sender=user) | (recipient=user)))
+
+        chatroom_mapper = []
+        for chatroom in chatrooms:
+            chatroom_dict = {}
+            chatroom_dict['pk'] = chatroom.pk
+
+            if user == chatroom.sender:
+                recipient = chatroom.recipient
+            else:
+                recipient = chatroom.sencder
+            chatroom_dict['recipient'] = recipient
+            chatroom_mapper.append(chatroom_dict)
+
+        return chatroom_mapper
+
+    def get_active_conversations(self, sender, recipient):
+        # 获取当前对话的消息列表
+        # Q 相当于SQL中的where
+        active_conversations = Message.objects.filter(
+                (Q(sender=sender) & Q(recipient=recipient)) |
+                (Q(sender=recipient) & Q(recipient=sender))
+                ).order_by('sent_at')
+
+        return active_conversations
+
+
--- direct_messages/views.py
+++ direct_messages/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.db.models import Q
+from django.views.generic import CreateView, RedirectView
 
-# Create your views here.
+from .models import Message, ChatRoom
+from .forms import MessageForm
+from .services import MessagingService
+
+
+@method_decorator([login_required], name='dispatch') # 装饰器限制权限
+class MessageView(RedirectView):
+    # 判断用户是否存在对话，若有则重定向至对话界面，无则重定向至任务列表页面
+    permanent = False
+    query_string = True
+    pattern_name = 'direct_messages:user_message'
+
+    def get_redirect_url(self, *args, **kwargs):
+        user = self.request.user
+        chatroom = ChatRoom.objects.filter(Q(sender=user) | Q(recipient=user)).first()
+        if chatroom:
+            return super().get_redirect_url(*args, pk=chatroom.pk)
+
+        return reverse('jobs:job_list')
